15a22b1357cba7d726f9da12fa7bfc0e311c7336
Altrino <albert.huang121@gmail.com>
2020-01-03 20:41:06 -0800

    delete

--- Queue/APIkeys.js
+++
@@ -1,4 +0,0 @@
-export const WEB_SOCKET="wss://hlxwa7203m.execute-api.us-west-2.amazonaws.com/Test";
-export const GET_ALL_DATA="https://5ch9sufu53.execute-api.us-west-2.amazonaws.com/testing/getcompanies";
-export const AUTH="https://5ch9sufu53.execute-api.us-west-2.amazonaws.com/testing/authenticate?code=";
-export const CROWD="https://5ch9sufu53.execute-api.us-west-2.amazonaws.com/testing/sendposition";
